57360d484d529779a89125c1279e85acfa14555b
MacOS <macos@MacOSs-MacBook-Pro.local>
2022-11-11 15:28:55 +0700

    demo

---
+++ demo.js
@@ -0,0 +1,79 @@
+// var app = new Vue({
+//   el: "#app",
+//   data: {
+//     message: "Hello every to day.",
+//   },
+// });
+
+// var app2 = new Vue({
+//   el: "#app-2",
+//   data: {
+//     message: "Bạn đã mở trang này vào " + new Date().toLocaleString(),
+//   },
+// });
+
+// var app3 = new Vue({
+//   el: "#app-3",
+//   data: {
+//     seen: true,
+//   },
+// });
+
+// var app4 = new Vue({
+//   el: "#app-4",
+//   data: {
+//     todos: [
+//       { text: "Học JavaScript" },
+//       { text: "Học Vue" },
+//       { text: "Xây dựng cái gì đó hay ho" },
+//     ],
+//   },
+// });
+
+// var app5 = new Vue({
+//   el: '#app-5',
+//   data: {
+//     message: 'qua lại khách chờ sông lặng sóng'
+//   },
+//   methods: {
+//     reverseMessage: function () {
+//       this.message = this.message.split(' ').reverse().join(' ')
+//     }
+//   }
+// })
+
+// var app6 = new Vue({
+//   el: "#app-6",
+//   data: {
+//     message: "Hãy sửa thông điệp này",
+//   },
+// });
+
+// Vue.component("todo-item", {
+//   props: ["todo"],
+//   template: "<li>{{ todo.text }}</li>",
+// });
+
+// var app7 = new Vue({
+//   el: "#app-7",
+//   data: {
+//     groceryList: [
+//       { id: 0, text: "Cà pháo" },
+//       { id: 1, text: "Mắm tôm" },
+//       { id: 2, text: "Miễn ăn được là được" },
+//     ],
+//   },
+// });
+
+// var date = { a: 1 };
+
+// var vm = new Vue({
+//   data: date,
+// });
+
+// // vm.a=date.a
+// // console.log(vm.a)
+// date.a = 2;
+// console.log(vm.a);
+
+// 
